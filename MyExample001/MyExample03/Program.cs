5770eb75dd5f617e12e760ea393812333c399886
AndreySozinov <savrey@ya.ru>
2022-01-30 23:45:27 +0500

    Add if example

---
+++ MyExample001/MyExample03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите свое имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "андрей")
+{
+    Console.WriteLine("Моё почтение, господин!");
+}
+else
+{
+    Console.Write("Ну, привет, ");
+    Console.WriteLine(username);
+}
+
+Console.WriteLine("Чему могу помочь?");
