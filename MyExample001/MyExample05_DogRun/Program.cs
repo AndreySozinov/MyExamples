fd52ddbbde75f254dca532a01315dd94587920c2
AndreySozinov <savrey@ya.ru>
2022-01-31 19:35:27 +0500

    Running Dog Task

---
+++ MyExample001/MyExample05_DogRun/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Сколько метров между друзьями?");
+int distance = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Какова скорость первого друга?");
+int speed1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Какова скорость второго друга?");
+int speed2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("А скорость собаки?");
+int dogspeed = Convert.ToInt32(Console.ReadLine());
+
+int friend = 2;
+int count = 0, 
+    time = 0;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance / (speed1 + dogspeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (speed2 + dogspeed);
+        friend = 1;
+    }
+    distance = distance - (speed1 + speed2) * time;
+    count++;
+}
+
+Console.WriteLine($"Собака успеет пробежать {count} раз");
