79e5fac7e1828d1e114eaff6a825dbc34569c01a
AndreySozinov <savrey@ya.ru>
2022-01-31 15:34:06 +0500

    Add Max number search

---
+++ MyExample001/MyExample035/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int a = new Random().Next(0, 100);
+int b = new Random().Next(0, 100);
+int c = new Random().Next(0, 100);
+int d = new Random().Next(0, 100);
+int e = new Random().Next(0, 100);
+
+Console.Write(a);
+Console.Write(" ");
+Console.Write(b);
+Console.Write(" ");
+Console.Write(c);
+Console.Write(" ");
+Console.Write(d);
+Console.Write(" ");
+Console.WriteLine(e);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ MyExample001/MyExample04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+int xa = 1; ya = 1;
+    xb = 1; yb = 30;
+    xc = 80; yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine(".");
