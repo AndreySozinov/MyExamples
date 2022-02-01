8c634154798d107ba0fbf848e87e586f4a99196f
AndreySozinov <savrey@ya.ru>
2022-02-01 22:44:09 +0500

    Add Function and Array

---
+++ MyExample001/MyExample06_Array/Program.cs
@@ -0,0 +1,38 @@
+﻿// Найти максимальное число из массива при помощи функции
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+void FillArray(int[] collection) {
+    int len = collection.Length;
+    int index = 0;
+    while (index < len) {
+        collection[index] = new Random().Next(1, 999);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) {
+    int count = col.Length;
+    int index = 0;
+    while (index < count) {
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+int[] arr = new int[9];
+FillArray(arr);
+PrintArray(arr);
+
+int result = Max(
+    Max(arr[0], arr[1], arr[2]), 
+    Max(arr[3], arr[4], arr[5]),
+    Max(arr[6], arr[7], arr[8])
+);
+Console.WriteLine();
+Console.WriteLine($"Максимальное число = {result}");
---
+++ MyExample001/MyExample06_function/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти максимальное число из девяти при помощи функции
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15, b1 = 20, c1 = 18,
+    a2 = 231, b2 = 5, c2 = 6,
+    a3 = 8, b3 = 1000, c3 = 10;
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
